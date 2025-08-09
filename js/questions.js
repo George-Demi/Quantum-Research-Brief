66a42d5d09e082562b294ed73227dde4562af57b
Georges Demirjian <georgesdemirdjians@gmail.com>
2025-08-09 16:11:27 +0300

    Update js/questions.js

--- js/questions.js
+++ js/questions.js
@@ -13,5 +13,15 @@ const questions = [
     question: "Who developed the theory of relativity?",
     options: ["Isaac Newton", "Albert Einstein", "Galileo", "Max Planck"],
     answer: "Albert Einstein"
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    options: ["Elephant", "Blue Whale", "Great White Shark", "Giraffe"],
+    answer: "Blue Whale"
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    options: ["Au", "Ag", "Pb", "Fe"],
+    answer: "Au"
   }
 ];
